513bdd28b30ceea4d90a97be3763506c0bf76fcb
Sean Sutton <arbor@Seans-MacBook-Pro.local>
2023-07-11 14:59:14 +0800

    Worked on bootstrapper

---
+++ data analysis/unobservables and switching/switcher_bootstrap_Males.R
@@ -0,0 +1,32 @@
+library(haven)
+library(tidyverse)
+library(ggplot2)
+
+dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(paste(dir, "../../cleaned_data/v3", sep="/"))
+
+income <- read_dta("male_private_resid.dta")
+# income$experience_cat <- cut(income$experience, breaks=c(0:3, seq(4,8,2), seq(10,40,5), 55))
+# table(income$experience_cat)
+
+income <- income %>%
+  group_by(wave, industry, occupation) %>%
+  mutate(subset_id = cur_group_id())
+
+income$idx <- 1:nrow(income)
+
+## The number of individuals with different numbers of observations in this sample
+table(data.frame(table(income$subset_id))$Freq)
+## Number of moves in the sample
+table(income$leading_moved_public)
+
+comparable_individuals = list()
+for (i in 1:sum(income$leading_moved_public)) {
+  row = income[income$leading_moved_public == 1,][i,]
+  group = row$subset_id
+  pred_wage = row$m5_pred
+  min_pred_wage = 0.95 * pred_wage
+  max_pred_wage = 1.05 * pred_wage
+  similar_indi = income[income$subset_id == group & income$m5_pred > min_pred_wage & income$m5_pred < max_pred_wage,]
+  comparable_individuals[[i]] = similar_indi$idx
+}
